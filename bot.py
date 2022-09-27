73ac5367422786c1a05039ba90e41dffd66556d2
Ilnur <ilnrzakirov@gmail.com>
2022-09-27 19:02:23 +0300

    Добавлен экземпляр бота и запуск

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 from aiogram import Bot, types
-from aiogram.dispatcher import dispatcher
+from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 import settings
 from loguru import logger
 
 logger.add("log.log", format="{time}, {level}, {message}", level="INFO", encoding="UTF-8")
+
+bot = Bot(token=settings.BOT_TOKEN)
+dispatcher = Dispatcher(bot)
+
+
+executor.start_polling(dispatcher, skip_updates=True)
 
