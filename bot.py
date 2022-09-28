0eb8b978597924639c9026c6a35efe7c1e6d95e1
Ilnur <ilnrzakirov@gmail.com>
2022-09-28 17:33:02 +0300

    Логирование хендлера старт

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
-from aiogram import Bot, types
+from aiogram import (
+    Bot,
+    types,
+)
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-import settings
 from loguru import logger
+
+import settings
 
 logger.add("log.log", format="{time}, {level}, {message}", level="INFO", encoding="UTF-8")
 
@@ -10,8 +14,10 @@ bot = Bot(token=settings.BOT_TOKEN)
 dispatcher = Dispatcher(bot)
 
 
+@logger.catch()
 @dispatcher.message_handler(commands=["start", "help"])
 async def echo(message: types.Message):
+    logger.info(f"Получена команда {message.text} от {message.from_user.username}")
     await message.answer("Hello")
 
 
