1569ef7157d5d54864869d36ce3c4119829c37be
Ilnur <ilnrzakirov@gmail.com>
2022-09-29 19:35:57 +0300

    Добавил временное хранилище

--- bot_init.py
+++ bot_init.py
@@ -1,7 +1,10 @@
 from aiogram import Bot
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
 
 import settings
 
+storage = MemoryStorage()
+
 bot = Bot(token=settings.BOT_TOKEN)
-dispatcher = Dispatcher(bot)
+dispatcher = Dispatcher(bot, storage=storage)
