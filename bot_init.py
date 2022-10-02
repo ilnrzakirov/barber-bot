0c50a73d11dd0ba70278ac87e68cb914cb81b251
Ilnur <ilnrzakirov@gmail.com>
2022-10-02 17:38:27 +0300

    Поменял стореж на редис

--- bot_init.py
+++ bot_init.py
@@ -1,10 +1,13 @@
 from aiogram import Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.dispatcher import Dispatcher
+from aioredis import Redis
 
 import settings
 
 storage = MemoryStorage()
+redis = Redis()
 
 bot = Bot(token=settings.BOT_TOKEN)
-dispatcher = Dispatcher(bot, storage=storage)
+dispatcher = Dispatcher(bot, storage=RedisStorage2())
